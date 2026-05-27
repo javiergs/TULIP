104eb7f1daa5cb89e960fc380b5c5d1ed2c012e0
Javier Gonzalez <3814755+javiergs@users.noreply.github.com>
2026-05-27 01:53:39 -0700

    Update Main.java

--- examples/github-list-files/src/main/java/example/Main.java
+++ examples/github-list-files/src/main/java/example/Main.java
@@ -2,13 +2,26 @@ package example;
 
 import javiergs.tulip.github.GitHubHandler;
 
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        GitHubHandler gh = new GitHubHandler();
+        Properties p = new Properties();
+
+        try (InputStream in =
+                 Main.class.getClassLoader()
+                     .getResourceAsStream("tulip.properties")) {
+
+            if (in != null) {
+                p.load(in);
+            }
+        }
+        String token = p.getProperty("GITHUB_TOKEN");
+        GitHubHandler gh = new GitHubHandler(token);
 
         List<String> files =
             gh.listFilesRecursive(
